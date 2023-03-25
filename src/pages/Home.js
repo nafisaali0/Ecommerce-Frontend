761b68155c77e9a5d42c8b0d081b79dac7d5b111
Shahinur <shahinurislam30101999@gmail.com>
2023-03-25 14:23:38 +0600

    home-2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -36,8 +36,9 @@ const Home = () => {
                   <div className="small-banner-content position-absolute">
                     <h4>Best Sells</h4>
                     <h5>HP Paviloion</h5>
-                    <p>From $999 <br/> 42.3/month</p>
-                   
+                    <p>
+                      From $999 <br /> 42.3/month
+                    </p>
                   </div>
                 </div>
                 <div className="small-banner position-relative py-2">
@@ -49,8 +50,9 @@ const Home = () => {
                   <div className="small-banner-content position-absolute">
                     <h4>Best Sells</h4>
                     <h5>Smart Watch</h5>
-                    <p>From $999 <br/> 42.3/month</p>
-                    
+                    <p>
+                      From $999 <br /> 42.3/month
+                    </p>
                   </div>
                 </div>
                 <div className="small-banner position-relative py-2">
@@ -62,8 +64,9 @@ const Home = () => {
                   <div className="small-banner-content position-absolute">
                     <h4>New Arrivals</h4>
                     <h5>ipad S-13 pro</h5>
-                    <p>From $999 <br/> 42.3/month</p>
-                  
+                    <p>
+                      From $999 <br /> 42.3/month
+                    </p>
                   </div>
                 </div>
                 <div className="small-banner position-relative">
@@ -75,8 +78,55 @@ const Home = () => {
                   <div className="small-banner-content position-absolute py-2">
                     <h4>New Arrivals</h4>
                     <h5>Headphones</h5>
-                    <p>From $999 <br/> 42.3/month</p>
-                    
+                    <p>
+                      From $999 <br /> 42.3/month
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="services d-flex  align-items-center justify-content-between">
+                <div className="d-flex  align-items-center gap-15">
+                  <img src="/images/service.png" alt="service" />
+                  <div>
+                    <h6>Free Shipping</h6>
+                    <p className="mb-0">From all border over $1</p>
+                  </div>
+                </div>
+                <div className="d-flex  align-items-center gap-15">
+                  <img src="/images/service-02.png" alt="service-2" />
+                  <div>
+                    <h6>Daily Serprise Offer</h6>
+                    <p className="mb-0">Upto 25% savings</p>
+                  </div>
+                </div>
+                <div className="d-flex  align-items-center gap-15">
+                  <img src="/images/service-03.png" alt="service-3" />
+                  <div>
+                    <h6>Support 24/7</h6>
+                    <p className="mb-0">Shop with an expert</p>
+                  </div>
+                </div>
+                <div className="d-flex  align-items-center gap-15">
+                  <img src="/images/service-04.png" alt="service-4" />
+                  <div>
+                    <h6>Affordable Price</h6>
+                    <p className="mb-0">Get Factory direct Price</p>
+                  </div>
+                </div>
+                <div className="d-flex  align-items-center gap-15">
+                  <img src="/images/service-05.png" alt="service-5" />
+                  <div>
+                    <h6>Secure Payment</h6>
+                    <p className="mb-0">100% Protected Payment</p>
                   </div>
                 </div>
               </div>
